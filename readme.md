146ff4db6fbdcc25ea22d8354862a5ec16bdabe3
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2025-02-07 11:46:21 +0700

    Completed problem 3

--- readme.md
+++ readme.md
@@ -1,10 +1,5 @@
-# 99Tech Code Challenge #1 #
+For problem 1: At root directory, run npx ts-node src/problem1/solution.ts to view result of problem 1 functions
 
-Note that if you fork this repository, your responses may be publicly linked to this repo.  
-Please submit your application along with the solutions attached or linked.   
+For problem 2: At root directory, move into the problem2 folder with cd src/problem2, install all dependencies with npm install, run project with npm run dev, view at http://localhost:5173
 
-It is important that you minimally attempt the problems, even if you do not arrive at a working solution.
-
-## Submission ##
-You can either provide a link to an online repository, attach the solution in your application, or whichever method you prefer.
-We're cool as long as we can view your solution without any pain.
+For problem 3: Answer.txt contains all suggested changes, FixedCode.tsx contains the enhanced code
---
+++ src/problem3/FixedCode.tsx
@@ -0,0 +1,86 @@
+interface WalletBalance {
+  currency: string;
+  amount: number;
+  blockchain: string;
+}
+interface FormattedWalletBalance extends WalletBalance {
+  formatted: string;
+}
+
+interface Props extends BoxProps {}
+
+const WalletPage: React.FC<Props> = (props: Props) => {
+  const { ...rest } = props;
+
+  const balances = useWalletBalances();
+  const prices = usePrices();
+
+  const getPriority = (blockchain: string): number => {
+    switch (blockchain) {
+      case "Osmosis":
+        return 100;
+      case "Ethereum":
+        return 50;
+      case "Arbitrum":
+        return 30;
+      case "Zilliqa":
+        return 20;
+      case "Neo":
+        return 20;
+      default:
+        return -99;
+    }
+  };
+
+  const sortedBalances = useMemo(() => {
+    return balances
+      .filter((balance: WalletBalance) => {
+        // const balancePriority = getPriority(balance.blockchain);
+
+        // Define/compute lhsPriority here
+        // const lhsPriority = ...
+
+        if (lhsPriority > -99) {
+          if (balance.amount <= 0) {
+            return true;
+          }
+        }
+        return false;
+      })
+      .sort((lhs: WalletBalance, rhs: WalletBalance) => {
+        const leftPriority = getPriority(lhs.blockchain);
+        const rightPriority = getPriority(rhs.blockchain);
+        if (leftPriority > rightPriority) {
+          return -1;
+        } else if (rightPriority > leftPriority) {
+          return 1;
+        } else return 0;
+      });
+  }, [balances]);
+
+  //   const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+  //     return {
+  //       ...balance,
+  //       formatted: balance.amount.toFixed(),
+  //     };
+  //   });
+
+  const rows = sortedBalances.map(
+    (balance: FormattedWalletBalance, index: number) => {
+      const usdValue = prices[balance.currency] ?? 0 * balance.amount;
+
+      // ideally WalletBalance should contain an id property, but this should do
+      return (
+        <WalletRow
+          className={classes.row}
+          key={`${balance.currency}-${index}`}
+          amount={balance.amount}
+          usdValue={usdValue}
+          formattedAmount={balance.formatted}
+        />
+      );
+    }
+  );
+
+  return <div {...rest}>{rows}</div>;
+};
